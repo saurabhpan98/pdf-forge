d6cbd1ad5275a5d4eae6c88facf0920643299ce5
Saurabh Panchal <40832570+saurabhpan98@users.noreply.github.com>
2026-09-12 17:53:33 +0530

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/img_pdf_app/',
+  base: '/pdf-forge/',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
