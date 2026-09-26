2af1d8efa429561b951efc2714e5fb0126c02a49
Saurabh Panchal <40832570+saurabhpan98@users.noreply.github.com>
2026-09-26 08:47:34 +0000

    feat: update

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+const base = process.env.VITE_BASE_PATH || '/'
+
 export default defineConfig({
-  base: '/pdf-forge/',
+  base,
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
