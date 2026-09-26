3607b537dc09de026d2fdd658faaedc2b3f5c374
Saurabh Panchal <40832570+saurabhpan98@users.noreply.github.com>
2026-09-26 14:39:09 +0530

    Set base path to '/pdf-forge/' in Vite config

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const base = process.env.VITE_BASE_PATH || '/'
+//const base = process.env.VITE_BASE_PATH || '/'
 
 export default defineConfig({
-  base,
+  base: '/pdf-forge/',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
